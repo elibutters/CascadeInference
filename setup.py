3accbbe34ff51734d0a086dcd4d387209f52da3f
Eli Butters <elibuttersover@gmail.com>
2025-06-16 18:51:01 -0500

    official release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cascade-inference",
-    version="0.0.5",
+    version="0.1.0",
     author="Eli Butters",
     author_email="elibutters@outlook.com",
     description="Cascade based inference for large language models.",
